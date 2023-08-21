712430d1726c9ec6b1838c0893f26a200a98e4d4
glauber castro miranda <glauber.happy@gmail.com>
2023-08-21 16:38:37 -0300

    "init commit"

--- GruntFile.js
+++ GruntFile.js
@@ -60,7 +60,7 @@ module.exports = function (grunt) {
                         },
                         {
                             match: 'ENDEREÇO_DO_JS',
-                            replacement: '../src/scripts/main.js'
+                            replacement: '../dist/scripts/main.min.js'
                         }
                     ]
                 },
